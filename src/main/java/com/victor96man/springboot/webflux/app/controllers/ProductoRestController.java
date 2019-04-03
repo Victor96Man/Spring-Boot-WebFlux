82877bd0ea1b48e7b133debaa3eb5a3025c3db43
Victor96Man <victormanuelgomu@gmail.com>
2019-04-03 13:29:15 +0200

    ApiRest Flux

---
+++ src/main/java/com/victor96man/springboot/webflux/app/controllers/ProductoRestController.java
@@ -0,0 +1,30 @@
+package com.victor96man.springboot.webflux.app.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.victor96man.springboot.webflux.app.models.dao.IProductoDao;
+import com.victor96man.springboot.webflux.app.models.documents.Producto;
+
+import reactor.core.publisher.Flux;
+
+@RestController
+@RequestMapping("/api/productos")
+public class ProductoRestController {
+
+	@Autowired
+	private IProductoDao dao;
+	
+	@GetMapping
+	public Flux<Producto> index(){
+		Flux<Producto> productos = dao.findAll().map(producto -> {
+			producto.setNombre(producto.getNombre().toUpperCase());
+			return producto;
+		}).doOnNext(prod -> System.out.println(prod.getNombre()));
+		
+		return productos;
+	}
+	
+}
