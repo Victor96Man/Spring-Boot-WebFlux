4610c700dcdeb6bbe7c98a46adbe5c831a13b280
Victor96Man <victormanuelgomu@gmail.com>
2019-04-03 13:30:14 +0200

    ApiRest mostrar un producto con Mono

--- src/main/java/com/victor96man/springboot/webflux/app/controllers/ProductoRestController.java
+++ src/main/java/com/victor96man/springboot/webflux/app/controllers/ProductoRestController.java
@@ -2,6 +2,7 @@ package com.victor96man.springboot.webflux.app.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +10,7 @@ import com.victor96man.springboot.webflux.app.models.dao.IProductoDao;
 import com.victor96man.springboot.webflux.app.models.documents.Producto;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/api/productos")
@@ -25,6 +27,12 @@ public class ProductoRestController {
 		}).doOnNext(prod -> System.out.println(prod.getNombre()));
 		
 		return productos;
+	}
+
+	@GetMapping("/{id}")
+	public Mono<Producto> show(@PathVariable String id){
+		Mono<Producto> producto = dao.findById(id);
+		return producto;
 	}
 	
 }
